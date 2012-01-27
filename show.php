9c71d393f9d4db114d2877197034568d4375657e
Michal Zugec <mzugec@gmail.com>
2012-01-27 12:59:34 +0100

    CSV export

--- show.php
+++ show.php
@@ -2,19 +2,29 @@
 error_reporting(E_STRICT|E_ALL);
 //$file = fopen("http://radio.data.free.fr/live_datas/Vsetin_112011rmob.TXT", 'r');
 //defined($_GET["file"]) or die("Filename must be selected");
-$file = fopen($_GET["file"], 'r');
+$file = fopen("../../smrst/rmob/".$_GET["file"], 'r');
 if (!$file) {
     echo "<p>Unable to open remote file.\n";
     exit;
 }
 
+Class ReportType {
+    const Daily   = 0;
+	const Monthly = 1;
+	const CSV	  = 2;
+}
+
+
 switch($_GET["type"]){
     case "daily":
-        $show_daily_report=1;
+        $report = ReportType::Daily;
         break;
     case "overview":
-        $show_daily_report=0;
+        $report = ReportType::Monthly;
         break;
+	case "csv":
+        $report = ReportType::CSV;	
+		break;
     default:
         die("Unknown preview type.");
 }
@@ -34,7 +44,7 @@ while (!feof ($file)) {
     }
     if ($line_num<=31)
     {
-        if (!$show_daily_report)
+        if ($report == ReportType::Monthly)
         {
             $line_data = preg_split("/[|]+/", $line);
             foreach($line_data as $value)
@@ -54,28 +64,50 @@ while (!feof ($file)) {
     }
 }
 
-if ($show_daily_report)
-{
-    // show information
-    foreach($info as $line)
-    {
-        echo "<br>".$line;
-    }
-    // show daily graphs
-    foreach($data as $line)
-    {
-        if (!strstr($line, "?"))
-        {
-           echo "<img src='daily.php?data=".urlencode(trim($line))."'>";
-        }
-    }
-}
-else
-{
-    require "overview.php";
-    $overview = new Overview();
-    $overview->import($data);
-    $overview->main($max_value);
+switch ($report){
+	case(ReportType::Daily):
+		// show information
+		foreach($info as $line)
+		{
+			echo "<br>".$line;
+		}
+		// show daily graphs
+		foreach($data as $line)
+		{
+			if (!strstr($line, "?"))
+			{
+				echo "<img src='daily.php?data=".urlencode(trim($line))."'>";
+			}
+		}
+		break;
+
+	case(ReportType::Monthly):
+		require "overview.php";
+		$overview = new Overview();
+		$overview->import($data);
+		$overview->main($max_value);
+		break;
+
+	case(ReportType::CSV):
+//		header('Content-Type: text/csv');
+		// FIXME: month
+		echo ",00h,01h,02h,03h,04h,05h,06h,07h,08h,09h,10h,11h,12h,13h,14h,15h,16h,17h,18h,19h,20h,21h,22h,23h<br>";
+		foreach($data as $line) 
+		{
+			// FIXME: 
+			$array_line = preg_split("/[|]+/", $line);
+			foreach($array_line as $value)
+			{
+				$value = trim($value);
+				echo is_numeric($value) ? $value : "";
+				echo ","; // separator
+			}
+			echo "<br>";
+		}
+		break;
+
+	default:
+		die("Unknown type");
 }
 
 fclose($file);
